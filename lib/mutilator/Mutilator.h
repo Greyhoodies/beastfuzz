4bb99f2b2d427827e465ed8a871f43e83b3093ba
hjubb <hjubb93@gmail.com>
2016-04-25 16:20:26 +1000

    Created Mutilator .h/.cpp

---
+++ lib/mutilator/Mutilator.h
@@ -0,0 +1,17 @@
+//
+// Created by jubb on 25/04/16.
+//
+
+#ifndef BEASTFUZZ_MUTILATOR_H
+#define BEASTFUZZ_MUTILATOR_H
+
+
+class Mutilator {
+private:
+    
+public:
+
+};
+
+
+#endif //BEASTFUZZ_MUTILATOR_H
